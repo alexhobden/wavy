ebff67bb2019f8802dfce82a4ba5b3b48d8fce8a
alexhobden <alex.hobden@gmx.de>
2024-09-05 17:00:16 +0200

    Add Tailwind to the Project

--- src/components/wavyTaskBar.tsx
+++ src/components/wavyTaskBar.tsx
@@ -1,7 +1,11 @@
 import style from "./wavy.module.css"
 
 const WavyTaskBar: React.FC = () => {
-    return <div className={style.taskbar}>hallo</div>
+    return (
+        <div className="fixed p-[3px] w-full bottom-0 left-0 h-10 wavy-solid">
+            hallo
+        </div>
+    )
 }
 
 export default WavyTaskBar
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        function ({ addComponents }) {
+            addComponents({
+                ".wavy-solid": {
+                    backgroundColor: "#ffe8a9",
+                    boxShadow: `
+                      inset -2px -2px 0 0 #443d2d,
+                      inset 2px 2px 0 0 #fffaee,
+                      inset -4px -4px 0 0 #c4b795,
+                      inset 4px 4px 0 0 #ffeec0
+                    `,
+                },
+                ".wavy-hollow": {
+                    backgroundColor: "#ffffff",
+                    boxShadow: `
+                      inset -2px -2px 0 0 #fffaee,
+                      inset 2px 2px 0 0 #443d2d,
+                      inset -4px -4px 0 0 #ffeec0,
+                      inset 4px 4px 0 0 #c4b795
+                `,
+                },
+            })
+        },
+    ],
+}
