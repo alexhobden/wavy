ba95ec528c027fa892c21f0d8f4ce8d46dc1f9ab
alexhobden <alex.hobden@gmx.de>
2024-09-05 17:06:58 +0200

    add font to tailwind

--- src/components/wavyTaskBar.tsx
+++ src/components/wavyTaskBar.tsx
@@ -2,7 +2,7 @@ import style from "./wavy.module.css"
 
 const WavyTaskBar: React.FC = () => {
     return (
-        <div className="fixed p-[3px] w-full bottom-0 left-0 h-10 wavy-solid">
+        <div className="font-wavy font-custom fixed p-[3px] w-full bottom-0 left-0 h-10 wavy-solid">
             hallo
         </div>
     )
