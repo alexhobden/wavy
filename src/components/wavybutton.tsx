cf94b8fa58e50f7b8164796382f4455b2d03b59b
alexhobden <alex.hobden@gmx.de>
2024-09-06 10:12:24 +0200

    almost fix buttons

--- src/components/wavybutton.tsx
+++ src/components/wavybutton.tsx
@@ -13,19 +13,19 @@ const WavyButton: React.FC<WavyButtonProps> = ({ children, funktion, use }) => {
     return (
         <button
             onClick={use}
-            className=" wavy-button w-[28px] h-[28px] ml-[1px] flex items-center justify-center"
+            className="w-[25px] h-[25px] ml-[1px] flex items-center justify-center"
             style={{
                 backgroundImage: `url(${name})`,
                 backgroundRepeat: `no-repeat` /* Prevent the SVG from repeating */,
                 backgroundPosition: `center`,
                 backgroundColor: "#ffe8a9",
                 boxShadow: `
-                    inset -4px -4px 0 0 #000000,
-                    inset 4px 4px 0 0 #000000,
-                    inset -1px -1px 0 0 #443d2d,
-                    inset 1px 1px 0 0 #fffaee,
-                    inset -2px -2px 0 0 #c4b795,
-                    inset 2px 2px 0 0 #fff2d0
+                    inset -2px -2px 0 0 #000000,
+                    inset 2px 2px 0 0 #000000,
+                    inset -3px -3px 0 0 #443d2d,
+                    inset 3px 3px 0 0 #fffaee,
+                    inset -4px -4px 0 0 #c4b795,
+                    inset 4px 4px 0 0 #fff2d0
             
                 `.trim(),
 
