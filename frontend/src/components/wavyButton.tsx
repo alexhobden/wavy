97de4c70f88af2d1093b6b24c81a9e2d9743da5f
alexhobden <alex.hobden@gmx.de>
2024-09-06 11:02:55 +0200

    move frontend into seperate folder

--- frontend/src/components/wavyButton.tsx
+++ frontend/src/components/wavyButton.tsx
@@ -15,20 +15,10 @@ const WavyButton: React.FC<WavyButtonProps> = ({ children, funktion, use }) => {
             onClick={use}
             style={{
                 backgroundImage: `url(${name})`,
-                backgroundRepeat: `no-repeat` /* Prevent the SVG from repeating */,
+                backgroundRepeat: `no-repeat`,
                 backgroundPosition: `center`,
-                // backgroundColor: "#ffe8a9",
-                // boxShadow: `
-                //     inset -2px -2px 0 0 #000000,
-                //     inset 2px 2px 0 0 #545454,
-                //     inset -3px -3px 0 0 #443d2d,
-                //     inset 3px 3px 0 0 #fffaee,
-                //     inset -4px -4px 0 0 #c4b795,
-                //     inset 4px 4px 0 0 #fff2d0
-
-                // `.trim(),
             }}
-            className="wavy-button w-[25px] h-[25px] ml-[1px] flex items-center justify-center"
+            className="wavy-button w-[24px] h-[24px] ml-[1px] flex items-center justify-center"
         >
             {children}
         </button>
